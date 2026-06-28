ab498be6ba8c42b98806d12108729ae911fc76d1
CharlotteHoyt <charlotte.hoyt@comcast.net>
2026-06-27 18:17:28 -0700

    Initial Commit

---
+++ KWK-Mobile-App-Challenge-2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KWK-Mobile-App-Challenge-2026
+//
+//  Created by Charlotte Hoyt on 6/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KWK-Mobile-App-Challenge-2026/KWK_Mobile_App_Challenge_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_Mobile_App_Challenge_2026App.swift
+//  KWK-Mobile-App-Challenge-2026
+//
+//  Created by Charlotte Hoyt on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct KWK_Mobile_App_Challenge_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

ca5756934c2bafc875e5ad3093bb2423346593b6
CharlotteHoyt <charlotte.hoyt@comcast.net>
2026-07-02 22:29:36 -0700

    Added a second view to project and added a navigation link from root view.

---
+++ KWK-Mobile-App-Challenge-2026/FlowersView.swift
@@ -0,0 +1,18 @@
+//
+//  FlowersView.swift
+//  KWK-Mobile-App-Challenge-2026
+//
+//  Created by Charlotte Hoyt on 7/2/26.
+//
+
+import SwiftUI
+
+struct FlowersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlowersView()
+}

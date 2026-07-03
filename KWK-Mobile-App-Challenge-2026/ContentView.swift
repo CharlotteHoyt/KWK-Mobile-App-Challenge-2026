acf3c02a93ed69c7f3b8842ca6ac488042c46b1f
CharlotteHoyt <charlotte.hoyt@comcast.net>
2026-07-02 18:25:15 -0700

    Added a VStack with title text, an image, and a link for practice.

--- KWK-Mobile-App-Challenge-2026/ContentView.swift
+++ KWK-Mobile-App-Challenge-2026/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, World!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.purple)
+            Image("fish")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+            Link(destination: URL(string: "https://www.seattleaquarium.org")!) {
+                Text("I saw these fish at the Seattle Aquarium!")
+            }
         }
         .padding()
     }
